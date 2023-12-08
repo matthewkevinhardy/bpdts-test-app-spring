c6e8c57db0c85dcb1c182cd181826a13de5b1c75
matthew <matthew@DESKTOP-CSM534J>
2023-12-08 13:32:37 +0000

    updates

--- src/main/java/bpdts/utils/NameFileService.java
+++ src/main/java/bpdts/utils/NameFileService.java
@@ -1,5 +1,6 @@
 package bpdts.utils;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
@@ -8,9 +9,7 @@ import javax.annotation.PostConstruct;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.core.io.Resource;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -23,19 +22,13 @@ public class NameFileService {
 
 	private ObjectMapper mapper = new ObjectMapper();
 
-	@Autowired
-	private ApplicationContext appContext;
-
-	// @Value("classpath:names.json")
-	// private Resource namesFile;
-
 	private List<User> users;
 
 	@PostConstruct
 	private void mapUserList() throws IOException {
-		Resource namesFile = appContext.getResource("classpath:names.json");
+		File namesFile = new ClassPathResource("names.json").getFile();
 
-		users = Collections.unmodifiableList(mapper.readerForListOf(User.class).readValue(namesFile.getFile()));
+		users = Collections.unmodifiableList(mapper.readerForListOf(User.class).readValue(namesFile));
 		LOG.info("Found users: " + users.size());
 	}
 
